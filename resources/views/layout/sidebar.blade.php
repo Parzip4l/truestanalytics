0de90797dccdb69277204fab09ec0c579a92576b
sobirinjamo33 <sobirinjamo33@gmail.com>
2024-05-29 10:46:48 +0700

    Updaet

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -1,7 +1,7 @@
 <nav class="sidebar">
   <div class="sidebar-header">
-    <a href="#" class="sidebar-brand">
-      Noble<span>UI</span>
+    <a href="#" class="sidebar-brand" style="width:90%">
+      <img src="{{ asset('/logo.png') }}" alt="" style="max-width:65%">
     </a>
     <div class="sidebar-toggler not-active">
       <span></span>
