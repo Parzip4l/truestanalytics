fe2dbf3228b5ad169d53ff43b3c470af9d56a34e
sobirinjamo33 <sobirinjamo33@gmail.com>
2024-05-29 15:52:20 +0700

    Update Data Count Visitor

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,33 +12,6 @@
                     <div class="card-body">
                         <div class="d-flex justify-content-between align-items-baseline">
                             <h6 class="card-title mb-0">Page Views</h6>
-                            <div class="dropdown mb-2">
-                                <button class="btn btn-link p-0" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <i class="icon-lg text-muted pb-3px" data-feather="more-horizontal"></i>
-                                </button>
-                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                    <a class="dropdown-item d-flex align-items-center" href="javascript:;">
-                                        <i data-feather="eye" class="icon-sm me-2"></i>
-                                        <span class="">View</span>
-                                    </a>
-                                    <a class="dropdown-item d-flex align-items-center" href="javascript:;">
-                                        <i data-feather="edit-2" class="icon-sm me-2"></i>
-                                        <span class="">Edit</span>
-                                    </a>
-                                    <a class="dropdown-item d-flex align-items-center" href="javascript:;">
-                                        <i data-feather="trash" class="icon-sm me-2"></i>
-                                        <span class="">Delete</span>
-                                    </a>
-                                    <a class="dropdown-item d-flex align-items-center" href="javascript:;">
-                                        <i data-feather="printer" class="icon-sm me-2"></i>
-                                        <span class="">Print</span>
-                                    </a>
-                                    <a class="dropdown-item d-flex align-items-center" href="javascript:;">
-                                        <i data-feather="download" class="icon-sm me-2"></i>
-                                        <span class="">Download</span>
-                                    </a>
-                                </div>
-                            </div>
                         </div>
                         <div class="row">
                             <div class="col-6 col-md-12 col-xl-5">
@@ -67,33 +40,6 @@
                     <div class="card-body">
                         <div class="d-flex justify-content-between align-items-baseline">
                             <h6 class="card-title mb-0">Visitor</h6>
-                            <div class="dropdown mb-2">
-                                <button class="btn btn-link p-0" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <i class="icon-lg text-muted pb-3px" data-feather="more-horizontal"></i>
-                                </button>
-                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                                    <a class="dropdown-item d-flex align-items-center" href="javascript:;">
-                                        <i data-feather="eye" class="icon-sm me-2"></i>
-                                        <span class="">View</span>
-                                    </a>
-                                    <a class="dropdown-item d-flex align-items-center" href="javascript:;">
-                                        <i data-feather="edit-2" class="icon-sm me-2"></i>
-                                        <span class="">Edit</span>
-                                    </a>
-                                    <a class="dropdown-item d-flex align-items-center" href="javascript:;">
-                                        <i data-feather="trash" class="icon-sm me-2"></i>
-                                        <span class="">Delete</span>
-                                    </a>
-                                    <a class="dropdown-item d-flex align-items-center" href="javascript:;">
-                                        <i data-feather="printer" class="icon-sm me-2"></i>
-                                        <span class="">Print</span>
-                                    </a>
-                                    <a class="dropdown-item d-flex align-items-center" href="javascript:;">
-                                        <i data-feather="download" class="icon-sm me-2"></i>
-                                        <span class="">Download</span>
-                                    </a>
-                                </div>
-                            </div>
                         </div>
                         <div class="row">
                             <div class="col-6 col-md-12 col-xl-5">
